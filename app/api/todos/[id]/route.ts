da76c1449208d6775cc013c270cb55bc02ce6371
dogunfx <dogunfx@gmail.com>
2025-04-22 17:09:54 +0100

    updated route

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -1,12 +1,15 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
+// App Router API handlers use this type for context
 export async function DELETE(
-  _: Request,
-  { params }: { params: { id: string } }
+  req: Request,
+  context: { params: { id: string } }
 ) {
+  const id = parseInt(context.params.id);
   await prisma.todo.delete({
-    where: { id: Number(params.id) },
+    where: { id },
   });
+
   return NextResponse.json({ success: true });
 }
